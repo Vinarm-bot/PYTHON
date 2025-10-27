63028b31506864ee51c757e9c1aaecdf2ccf322d
Vinarm verma <vinarmverma95@gmail.com>
2025-10-27 22:46:29 +0530

    Add files via upload

---
+++ task_02.py
@@ -0,0 +1,13 @@
+
+
+
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+
+full_name = first_name + " " + last_name
+
+
+print("\n--- Greeting Message ---")
+print(f"Hello, {full_name}! Welcome to Python programming 😊")
